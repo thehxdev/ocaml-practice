63ff68414a36f5628afbdf36050cffbf4b3dbaf8
thehxdev <hossein.khosravi.ce@gmail.com>
2024-09-22 21:00:09 +0330

    code formating
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- lib/list.ml
+++ lib/list.ml
@@ -1,5 +1,5 @@
 (* get lat element of a list *)
-let rec last (l: 'a list): 'a option =
+let rec last (l: 'a list) : 'a option =
     match l with
     | [] -> None
     | [x] -> Some x
@@ -7,7 +7,7 @@ let rec last (l: 'a list): 'a option =
 ;;
 
 (* get last two elements of a list *)
-let rec last_two (l: 'a list): ('a * 'a) option =
+let rec last_two (l: 'a list) : ('a * 'a) option =
     match l with
     | [] -> None
     | [_] -> None
@@ -16,15 +16,15 @@ let rec last_two (l: 'a list): ('a * 'a) option =
 ;;
 
 (* get nth element of a list *)
-let rec nth (l: 'a list) (k: int): 'a option =
+let rec nth (l: 'a list) (k: int) : 'a option =
     match l with
     | [] -> None
     | x :: xs -> if k == 0 then Some x else nth xs (k-1)
 ;;
 
 (* calculate length of the list *)
-let len (l: 'a list): int =
-    let rec list_len' (l: 'a list) (i: int): int =
+let len (l: 'a list) : int =
+    let rec list_len' (l: 'a list) (i: int) : int =
         match l with
         | [] -> i
         | _ :: xs -> list_len' xs (i+1)
@@ -33,8 +33,8 @@ let len (l: 'a list): int =
 ;;
 
 (* reverse a list *)
-let reverse (l: 'a list): 'a list =
-    let rec reverse' (l: 'a list) (acc: 'a list): 'a list =
+let reverse (l: 'a list) : 'a list =
+    let rec reverse' (l: 'a list) (acc: 'a list) : 'a list =
         match l with
         | [] -> acc
         | x :: xs -> reverse' xs (x :: acc)
@@ -43,15 +43,15 @@ let reverse (l: 'a list): 'a list =
 ;;
 
 (* TODO: solve is_palindrome *)
-let is_palindrome (_l: 'a list): bool =
+let is_palindrome (_l: 'a list) : bool =
     false
 ;;
 
 (* tail recursive map *)
 (** This version of map function is optimized (O n) but the result is in reversed order
     If you want the right order, call reverse (O n^2) *)
-let map (f: ('a -> 'b)) (l: 'a list): 'b list =
-    let rec map' (f: ('a -> 'b)) (l: 'a list) (acc: 'b list): 'b list =
+let map (f: ('a -> 'b)) (l: 'a list) : 'b list =
+    let rec map' (f: ('a -> 'b)) (l: 'a list) (acc: 'b list) : 'b list =
         match l with
         | [] -> acc
         | x :: xs -> map' f xs (f x :: acc)
@@ -62,8 +62,8 @@ let map (f: ('a -> 'b)) (l: 'a list): 'b list =
 (* tail recursive filter *)
 (** This version of filter function is optimized (O n) but the result is in reversed order
     If you want the right order, call reverse (O n^2) *)
-let filter (f: ('a -> bool)) (l: 'a list): 'a list =
-    let rec filter' (f: ('a -> bool)) (l: 'a list) (acc: 'a list): 'a list =
+let filter (f: ('a -> bool)) (l: 'a list) : 'a list =
+    let rec filter' (f: ('a -> bool)) (l: 'a list) (acc: 'a list) : 'a list =
         match l with
         | [] -> acc
         | x :: xs -> if (f x) then filter' f xs (x :: acc) else filter' f xs acc
@@ -72,8 +72,8 @@ let filter (f: ('a -> bool)) (l: 'a list): 'a list =
 ;;
 
 (* tail recursive sum *)
-let sum (l: int list): int =
-    let rec sum' (l: int list) (acc: int): int =
+let sum (l: int list) : int =
+    let rec sum' (l: int list) (acc: int) : int =
         match l with
         | [] -> acc
         | x :: xs -> sum' xs (acc + x)
@@ -81,8 +81,8 @@ let sum (l: int list): int =
     sum' l 0
 ;;
 
-let range (l: int) (h: int): int list =
-    let rec range' (l: int) (h: int) (acc: int list): int list =
+let range (l: int) (h: int) : int list =
+    let rec range' (l: int) (h: int) (acc: int list) : int list =
         if h <= l then
             acc
         else
